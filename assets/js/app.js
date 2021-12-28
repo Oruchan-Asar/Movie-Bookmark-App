4b9328c97da5cd0fb31f37319ac006b1fdc6552f
Oruchan Asar <oruchan.asar@gmail.com>
2021-12-28 14:29:42 +0300

    Add omdb api and functionality

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,36 +1,39 @@
-let movies = [
-  {
-    title: "The Man From Earth 1",
-    description: `John üniversiteden ayrilip, başka bir yere gitmeye karar vermiş, akademisyen arkadaşlari da ona veda etmek için evine gelmişlerdir. Arkadaşlari John'u kalmasi için ikna etmeye çalişirken, John büyük sirrini ortaya dökecek ve herkesi büyük bir şaşkinliğa sürükleyecekti`,
-    poster:
-      "https://upload.wikimedia.org/wikipedia/en/3/3b/The_Man_from_Earth.png",
-    isFavourite: true,
-  },
-  {
-    title: "The Man From Earth 2",
-    description: `John üniversiteden ayrilip, başka bir yere gitmeye karar vermiş, akademisyen arkadaşlari da ona veda etmek için evine gelmişlerdir. Arkadaşlari John'u kalmasi için ikna etmeye çalişirken, John büyük sirrini ortaya dökecek ve herkesi büyük bir şaşkinliğa sürükleyecekti`,
-    poster:
-      "https://upload.wikimedia.org/wikipedia/en/3/3b/The_Man_from_Earth.png",
-    isFavourite: false,
-  },
-  {
-    title: "The Man From Earth 3",
-    description: `John üniversiteden ayrilip, başka bir yere gitmeye karar vermiş, akademisyen arkadaşlari da ona veda etmek için evine gelmişlerdir. Arkadaşlari John'u kalmasi için ikna etmeye çalişirken, John büyük sirrini ortaya dökecek ve herkesi büyük bir şaşkinliğa sürükleyecekti`,
-    poster:
-      "https://upload.wikimedia.org/wikipedia/en/3/3b/The_Man_from_Earth.png",
-    isFavourite: true,
-  },
-];
+const search_text = document.querySelector(".search_text");
+
+search_text.addEventListener("keydown", (event) => {
+  if (event.keyCode == 13) {
+    searchMovie();
+  }
+});
+
+async function searchMovie() {
+  const request = await fetch(
+    `http://www.omdbapi.com/?apikey=4de81934&s=${search_text.value}`
+  );
+  const data = await request.json();
+  let movies = data.Search.map((movie) => {
+    return {
+      title: movie.Title,
+      description: `${movie.Year}/${movie.Type}`,
+      imdbID: movie.imdbID,
+      poster:
+        movie.Poster === "N/A" ? "/assets/images/default.png" : movie.Poster,
+      isFavourite: false,
+    };
+  });
+  prepareMovies(movies);
+}
 
 function prepareMovies(movies) {
+  document.querySelector("#movies").innerHTML = "";
   movies.forEach((movie) => {
     let movie_card = document.createElement("movie-card");
     movie_card.setAttribute("title", movie.title);
     movie_card.setAttribute("poster", movie.poster);
+    movie_card.setAttribute("isFavourite", movie.isFavourite);
+    movie_card.setAttribute("imdbID", movie.imdbID);
     movie_card.innerHTML = movie.description;
 
     document.querySelector("#movies").append(movie_card);
   });
 }
-
-prepareMovies(movies);
--- components/movieCard/movieCard.js
+++ components/movieCard/movieCard.js
@@ -24,6 +24,8 @@ class MovieCard extends HTMLElement {
   constructor() {
     super();
 
+    this.isFavourite = false;
+
     this.attachShadow({ mode: "open" });
     this.shadowRoot.appendChild(template.content.cloneNode(true));
 
@@ -31,7 +33,48 @@ class MovieCard extends HTMLElement {
       this.shadowRoot.querySelector("h3.title").innerHTML =
         this.getAttribute("title");
       this.shadowRoot.querySelector("img").src = this.getAttribute("poster");
+      this.shadowRoot
+        .querySelector(".button")
+        .setAttribute(
+          "href",
+          `https://imdb.com/title/${this.getAttribute("imdbID")}`
+        );
+
+      if (this.getAttribute("isFavourite") == "true") {
+        this.isFavourite = true;
+        this.shadowRoot
+          .querySelector(".isFavourite")
+          .classList.add("is_favourite");
+      }
     }, 100);
+  }
+
+  favToggle() {
+    this.isFavourite = !this.isFavourite;
+
+    if (this.isFavourite) {
+      this.shadowRoot
+        .querySelector(".isFavourite")
+        .classList.add("is_favourite");
+    } else {
+      this.shadowRoot
+        .querySelector(".isFavourite")
+        .classList.remove("is_favourite");
+    }
+  }
+
+  connectedCallback() {
+    this.shadowRoot
+      .querySelector(".isFavourite")
+      .addEventListener("click", () => {
+        this.favToggle();
+      });
+  }
+
+  disconnectedCallback() {
+    this.shadowRoot
+      .querySelector(".isFavourite")
+      .removeEventListener("click", this.favToggle());
   }
 }
 
